fd64f60db119167debeefc8ce4e1d375c7211f2f
alessidaniele <alessi.daniele1994@gmail.com>
2024-03-14 15:27:31 +0100

    aggior

---
+++ _personale/esercizio mio.py
@@ -0,0 +1,13 @@
+saldo_iniziale= 1000
+INTERESSE = 1.10
+interesse1 = saldo_iniziale * INTERESSE
+interesse2 = interesse1 * INTERESSE
+interesse3 = interesse2 * INTERESSE
+
+print("saldo iniziale è:" , saldo_iniziale)
+print("saldo al secondo anno è:" , interesse1)
+print("saldo al terzo anno è:" , interesse2)
+print("saldo al quarto anno è:" , interesse3)
+
+
+
---
+++ _personale/frutti2.py
@@ -0,0 +1,7 @@
+fruit1='mela' 
+fruit2='banana' 
+fruit3='cocco' 
+
+print(fruit1, fruit2, fruit3, sep=', ', end='. \n')
+print()
+print('Questi sono alcuni frutti!')
