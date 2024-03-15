9701bd70de091a29300567d9b1a5b321617186ca
alessidaniele <alessi.daniele1994@gmail.com>
2024-03-15 17:26:37 +0100

    agg

--- _personale/esercizio numeri.py
+++ _personale/esercizio numeri.py
@@ -1,5 +1,5 @@
-num1 = 20
-num2 = 10
+num1 = 16
+num2 = 27
 
 somma= num1 + num2
 differenza= num1 - num2
@@ -8,23 +8,13 @@ media= (num1 + num2) / 2
 valore_assoluto= abs(differenza)
 valore_massimo= max(num1, num2)
 valore_minimo= min(num1, num2)
-print('la somma è: ',somma)
 
-print()
-
-print('la differenza è:', differenza)
-
-print()
-
-
+print('la somma è: ',somma, '\n')
+print('la differenza è:', differenza, '\n')
 print('il prodotto è:', prodotto, '\n')
-
 print('la media del numero è:', media , '\n')
-
 print('il valore assoluto è:' , valore_assoluto , '\n')
-
 print('il valore massimo è:', valore_massimo, '\n')
-
 print('il numero minimo è:' , valore_minimo, '\n')
 
 
