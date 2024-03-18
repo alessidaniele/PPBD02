c0aa8e4848715addfb89c88ebbb53f514bb42e8f
alessidaniele <alessi.daniele1994@gmail.com>
2024-03-18 17:31:37 +0100

    aggior

--- _personale/esercizio numeri.py
+++ _personale/esercizio numeri.py
@@ -1,3 +1,5 @@
+# esercizio 1 
+
 num1 = 16
 num2 = 27
 
@@ -16,6 +18,31 @@ print('la media del numero è:', media , '\n')
 print('il valore assoluto è:' , valore_assoluto , '\n')
 print('il valore massimo è:', valore_massimo, '\n')
 print('il numero minimo è:' , valore_minimo, '\n')
+
+
+# esercizio 2 (len)
+print()
+
+mio_testo = 'ciao come stai oggi'
+mio_elenco = ('ciao', 'come', 'stai', 'oggi')
+
+print(len(mio_testo))
+print(len(mio_elenco))
+
+# esercizio 3 (type)
+print()
+
+print(type(1))
+print(type('ciao'))
+print(type(2.1))
+print(type(True))
+print(type(False))
+print(type(['ciao', 2]))
+print()
+
+#esercio 4 
+
+
 
 
 
